d70392fbf9995b6f74c171764d65093e3161d0ca
Adidev <crowlingspider@gmail.com>
2025-04-22 10:41:42 +0530

    started real work

--- scripts/firebase.js
+++ scripts/firebase.js
@@ -1,13 +1,8 @@
-
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-analytics.js";
 import { getDatabase, ref, set, get, child } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-database.js";
 import { onValue } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-database.js";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyB9ACEsLDEsech23PHoWJjMczUxGdhgIO0",
   authDomain: "football-tournament-450f7.firebaseapp.com",
@@ -40,20 +35,15 @@ export async function read(key) {
     }
   }
   
-      
-  document.addEventListener("DOMContentLoaded", () => {
-    const messageRef = ref(db, "demo/message");
-  
-    onValue(messageRef, (snapshot) => {
-      const data = snapshot.val();
-      const teama = document.getElementById("teamA-score");
-  
-      if (teama) {
-        console.log("🔥 Real-time update:", data);
-        teama.innerText = data.text;
-      } else {
-        console.log("Element not found: teamA-score");
-      }
-    });
-  });
+function handle(snapshot){
+    const data = snapshot.val();
+    console.log("🔥 Real-time update:", data);
+    console.log(data.teama.score)
+    console.log(data.teamb.score)
+}      
+document.addEventListener("DOMContentLoaded", () => {
+  const messageRef = ref(db, "Live/live");
+
+  onValue(messageRef,handle)
+});
   
--- scripts/other.js
+++ scripts/other.js
@@ -1,7 +1,7 @@
 import { read } from './firebase.js';
 
 async function fetchData() {
-  const data = await read('demo/message');
+  const data = await read('Live/live');
   console.log("🎉 Got data:", data);
 }
 
